08af754c2e70eb226f9f1830d5e4f20b5fa3e1b6
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-08-21 11:02:26 +0000

    Install Django, run migrations and create superuser

---
+++ bojeaux/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for bojeaux project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bojeaux.settings')
+
+application = get_wsgi_application()
