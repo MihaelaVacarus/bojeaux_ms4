411ebd313a9aa38504cae68c357f7463165b6ee3
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-09-07 18:59:10 +0000

    Add checkout views and templates

--- bojeaux/settings.py
+++ bojeaux/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 ]
 
 SITE_ID = 1
@@ -72,6 +73,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'bojeaux.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -86,8 +89,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
