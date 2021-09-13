a8f724e0084361a4512a4e93dbfe4ce552fda88e
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-09-13 20:34:12 +0000

    Deployment - part 2

--- bojeaux/settings.py
+++ bojeaux/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-2rc%o#=zpszc(-^pub9+e2zpm8o@n_nev9@ow77k)%v%_%e_=v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bojeaux-ms4.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -119,12 +119,17 @@ WSGI_APPLICATION = 'bojeaux.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
