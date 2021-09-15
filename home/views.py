9fcea47e5988cf8ae8c9e8f33dde981f81fcc7b5
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-09-15 20:14:41 +0000

    Add about template, view and url

--- home/views.py
+++ home/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """ A view to return the about page """
+
+    return render(request, 'home/about.html')
