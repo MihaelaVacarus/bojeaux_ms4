bc718ca9f9129380fa646f41c037c61a6c58638a
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-09-15 21:56:47 +0000

    Add privacy policy template, views, url and finish legal styling

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('about/', views.about, name='about'),
     path('terms_of_use/', views.terms_of_use, name='terms_of_use'),
+    path('privacy_policy/', views.privacy_policy, name='privacy_policy'),
 ]
--- home/views.py
+++ home/views.py
@@ -16,3 +16,9 @@ def terms_of_use(request):
     """ A view to return the terms of use page """
 
     return render(request, 'home/terms_of_use.html')
+
+
+def privacy_policy(request):
+    """ A view to return the privacy policy page """
+
+    return render(request, 'home/privacy_policy.html')
