7e982b99d70327ec267837a9b49b8786227e8d86
MihaelaVacarus <mihaela.vacarus@outlook.com>
2021-09-19 10:16:47 +0000

    Create individual blog post view and template

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name='blog'),
+    path('<slug:slug>', views.blog_post, name='blog_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Blog
 
@@ -6,10 +6,22 @@ from .models import Blog
 def blog(request):
     """ A view to see the blog page """
 
-    posts = Blog.objects.all()
+    posts = Blog.objects.all().order_by('-created_on')
 
     context = {
         'posts': posts,
     }
 
     return render(request, 'blog/blog.html', context)
+
+
+def blog_post(request, slug):
+    """ A view to see each blog post """
+
+    blog = get_object_or_404(Blog, slug=slug)
+
+    context = {
+        'blog': blog,
+    }
+
+    return render(request, 'blog/blog_post.html', context)
